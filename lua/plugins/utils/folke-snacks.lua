17de81b1e0c008e70c25f18a761ea7c23437002e
Croitor Tudor <t.croitor@almvaviva.it>
2024-12-14 11:03:11 +0100

    changed the keybinding for buffer delete to <C-d> instead of <leader>bdc (seemed too long)

--- lua/plugins/utils/folke-snacks.lua
+++ lua/plugins/utils/folke-snacks.lua
@@ -15,7 +15,7 @@ return {
     bufdelete = { enable = true }
   },
   keys = {
-    { "<leader>bdc", function() Snacks.bufdelete() end, desc = "[B]uffer [D]elete [C]urrent"},
+    { "<C-d>", function() Snacks.bufdelete() end, desc = "Delete current buffer"},
     { "<leader>bda", function() Snacks.bufdelete.all() end, desc = "[B]uffer [D]elete [A]ll"},
     { "<leader>bdo", function() Snacks.bufdelete.other() end, desc = "[B]uffer [D]elete [O]ther"},
   },
